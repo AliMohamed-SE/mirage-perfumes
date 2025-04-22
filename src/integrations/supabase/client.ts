7092778c15def2a4b307e05bd3bf4ec2fb1e69b1
AliMohamed-SE <alimohamed.deveng@gmail.com>
2025-04-22 23:16:09 +0200

    secrets added

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL = "https://pzxbypzxholtqfsdiysc.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6eGJ5cHp4aG9sdHFmc2RpeXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMzE5MzAsImV4cCI6MjA2MDgwNzkzMH0.1WJO4xv7AbLHRJCsg67LocldCd7HLF7e7BG69z4S_Pc";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
